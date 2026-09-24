bc91262a3a688bd238cc021f7f10a62750dc9ca3
ItsMeViipeR <itsmeviiper@gmail.com>
2026-09-24 14:24:25 +0000

    fix: c flake

--- c/flake.nix
+++ c/flake.nix
@@ -11,18 +11,16 @@
     flake-utils.lib.eachDefaultSystem (
       system:
       let
-	pkgs = nixpkgs.legacyPackages.${system};
+        pkgs = nixpkgs.legacyPackages.${system};
       in
       {
-	devShells.default = pkgs.mkShell {
-	  packages = with pkgs; [
-	    gcc
-	    gnumake
-	    git
-	  ];
-	};
-      };
-    }
-  );
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            gcc
+            gnumake
+            git
+          ];
+        };
+      }
+    );
 }
-
